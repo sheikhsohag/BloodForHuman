a4f4a9096a89af35aab0098a1f10b3ee8d61c731
sheikhsohag <sheikhsohag000@gmail.com>
2024-02-05 23:51:18 +0600

    start design base.html and create views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+class Home(View):
+    template_name = "home.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {"hello": "hello class base views"})
+    
+    def post(self, request, *args, **kwargs):
+        pass
